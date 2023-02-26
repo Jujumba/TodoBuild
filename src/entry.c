11c8f29b09e28156250486f9fded27b29884f6a3
Jujumba <far77578@gmail.com>
2023-02-27 00:39:14 +0100

    entry: add build, init, --help commands

---
+++ src/entry.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include "parser.h"
+#include <dirent.h>
+#include <stdlib.h>
+
+void compile();
+void create_target();
+void print_help();
+void init_project();
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Use a '--help' flag to see all available commands\n");
+        exit(EXIT_FAILURE);
+    }
+    if (!strcmp(argv[1], "--help")) {
+        print_help();
+    } else if (!strcmp(argv[1], "build")) {
+        parse();
+        compile();
+    } else if (!strcmp(argv[1], "init")) {
+        init_project();
+    } else {
+        printf("Unknown command. Please use a '--help' flag to see all available commands");
+    }
+    return 0;
+}
+
+void compile() {
+    extern char* query;
+    printf("Generated query: %s\n", query);
+    create_target();
+    chdir("target");
+    system(query);
+    free(query);
+}
+
+void create_target() {
+    DIR* target = opendir("target");
+    if (!target) {
+        system("mkdir target");
+    }
+    closedir(target);
+}
+
+void print_help() {
+    printf("Todo build system.\n\n"); // Seriously, I need to come up with a name
+    printf("Commands:\n");
+    printf("  build - build your project following with the rules described in build.todo\n");
+    printf("  init - create a build.todo in the working directory\n");
+}
+
+void init_project() {
+    FILE* build_rules = fopen("build.todo", "w");
+    fprintf(build_rules, "# Your journey start here...");
+}
