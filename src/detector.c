065fbb493adfa5e9dff3493d3693dd9ffb5ea40b
Jujumba <far77578@gmail.com>
2023-02-27 13:57:21 +0100

    Create compiler detector

---
+++ src/detector.c
@@ -0,0 +1,16 @@
+#include "detector.h"
+
+char *comp = NULL;
+
+bool detect() {
+    bool found;
+    printf("Detecting the C compiler...\n");
+    if (system("clang --version") == 0) {
+        comp = "clang";
+        found = true;
+    } else if (system("gcc --version") == 0) {
+        comp = "gcc";
+        found = true;
+    }
+    return found;
+}
---
+++ src/detector.h
@@ -0,0 +1,11 @@
+#ifndef BUILDSYS_DETECTOR_H
+#define BUILDSYS_DETECTOR_H
+
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "stdbool.h"
+
+bool detect();
+
+#endif // BUILDSYS_DETECTOR_H
